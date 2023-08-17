42efca7086090c9ff9f0f408e0b2d9bb1e507791
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-08-17 19:23:20 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- my-folder/problems/missing_number/solution.cpp
+++ my-folder/problems/missing_number/solution.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
+        int sum=(n)*(n+1)/2;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]!=i)
-            return i;
+            sum-=nums[i];
+
         }
-        return n;
+        return sum;
     }
 };
