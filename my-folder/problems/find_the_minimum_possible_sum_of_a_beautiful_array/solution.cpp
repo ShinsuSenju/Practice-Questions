5adc1cb848f332eae9ec4f6b990ef5209d9ab3d4
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-08-29 17:04:03 +0000

    Sync LeetCode submission - Find the Minimum Possible Sum of a Beautiful Array (cpp)

--- my-folder/problems/find_the_minimum_possible_sum_of_a_beautiful_array/solution.cpp
+++ my-folder/problems/find_the_minimum_possible_sum_of_a_beautiful_array/solution.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
     long long minimumPossibleSum(int n, int target) {
         set<int> s;
-        long long int cursum=0;
-        int cur=1;
+        long long cur=1,cursum=0;
         int i=0;
-        
-        while(i<n)
-        {
-            if(s.count(target-cur)<1){
-                s.insert(cur);
-                cursum+=cur;
-                cur++;
-                i++;
+        while(i<n){
+            if(s.count(target-cur)<1) {
+                
+            s.insert(cur);
+            cursum+=cur;
+            cur++;
+            i++;
             }
-            else
+            else{
                 cur++;
-                continue;
-            
-                
-                
+            }
         }
         return cursum;
-        
         
     }
 };
