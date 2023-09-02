248c25c86af80fbc64b386db4d8b6ff2daccd792
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-09-02 14:45:04 +0000

    Sync LeetCode submission - Check if Strings Can be Made Equal With Operations I (cpp)

---
+++ my-folder/problems/check_if_strings_can_be_made_equal_with_operations_i/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+       
+     
+        for(int i=0;i<4;i++)
+        { if(s1[i]==s2[i+2])
+        {
+            swap(s1[i],s1[i+2]);
+        }
+            
+            
+        }
+        return s1==s2;
+            
+            
+        
+    }
+};
