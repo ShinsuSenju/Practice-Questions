656e1792673292979cd1363448be80bc8f3f84eb
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-09-17 05:44:04 +0000

    Sync LeetCode submission - Happy Students (cpp)

---
+++ my-folder/problems/happy_students/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countWays(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=0,n = nums.size();
+        if(nums[0]>0) ans++;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]<i+1 && nums[i+1]>i+1) ans++;
+        }
+        if(nums.back()<n) ans++;
+        return ans;
+        
+    }
+};
