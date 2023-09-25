67d5b9f4247b2241f75dbdf33681d5b5417d4621
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-09-25 04:59:59 +0000

    Sync LeetCode submission - Find the Difference (cpp)

---
+++ my-folder/problems/find_the_difference/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum1=0,sum2=0;
+        for(int i=0;i<s.size();i++)
+            sum1+=s[i];
+    
+        for(int i=0;i<t.size();i++)
+            sum2+=t[i]; 
+
+        char ch = sum2-sum1;
+        return ch;
+
+    
+        
+    }
+};
