fe7748b3cfb5408e1572a0e1924b937ed8a0c767
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-09-28 05:29:41 +0000

    Sync LeetCode submission - Merge Sorted Array (cpp)

--- my-folder/problems/merge_sorted_array/solution.cpp
+++ my-folder/problems/merge_sorted_array/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int idx1=m-1,idx2=n-1,x=m+n-1;
-        while( idx1>=0 & idx2>=0 )
+        while( idx1>=0 && idx2>=0 )
         {
             if(nums1[idx1]>=nums2[idx2])
             {
