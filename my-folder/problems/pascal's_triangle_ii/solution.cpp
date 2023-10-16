8a9e59649c6bb4acaba0862c7a6ba948fd34800d
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-10-16 04:40:08 +0000

    Sync LeetCode submission - Pascal's Triangle II (cpp)

--- my-folder/problems/pascal's_triangle_ii/solution.cpp
+++ my-folder/problems/pascal's_triangle_ii/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    vector<int> getRow(int i) {
-        vector<int> ans;
-        i++;
-        long long res=1;
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> triangle;
+        triangle.push_back({1});
+        triangle.push_back({1,1});
+        for(int i=2;i<=rowIndex;i++){
+            vector<int> temp(i+1,1);
+            for(int j=1;j<i;j++)temp[j]= triangle[i-1][j]+triangle[i-1][j-1];
+            triangle.push_back(temp);
+
+        }
+        return triangle[rowIndex];
         
-        for(int j=1;j<=i;j++){
-                ans.push_back(res);
-                res=res*(i-j);
-                res=res/j;
-            }
-        return ans;
     }
 };
