546d9a6dc0c445c5ebf1f9a65e89fbcf319291c9
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-10-31 13:48:24 +0000

    Sync LeetCode submission - Subarrays Distinct Element Sum of Squares I (cpp)

--- my-folder/problems/subarrays_distinct_element_sum_of_squares_i/solution.cpp
+++ my-folder/problems/subarrays_distinct_element_sum_of_squares_i/solution.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int sumCounts(vector<int>& nums) {
     int ans = 0, count = 0;
-    for(int i = 0; i < nums.size(); ++i){
+    for(int i = 0; i < nums.size(); i++){
         unordered_map<int, int> ump; count = 0;
-        for(int j = i; j < nums.size(); ++j){
+        for(int j = i; j < nums.size(); j++){
             ump[nums[j]]++;
             if(ump[nums[j]] == 1) count++;
             ans += count*count;
