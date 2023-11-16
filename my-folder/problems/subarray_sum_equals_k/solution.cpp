2faff90d5726aa42748cb91660afd3e4dc6bfc3f
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2023-11-16 21:22:49 +0000

    Sync LeetCode submission - Subarray Sum Equals K (cpp)

--- my-folder/problems/subarray_sum_equals_k/solution.cpp
+++ my-folder/problems/subarray_sum_equals_k/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& a, int k) {
-        unordered_map<int,int> tmap;
-        int c=0,sum=0;
-
-        if(a.size()==1)
-        return a[0]==k;
-
-        for(int i=0;i<a.size();i++)
-        {
-            sum+=a[i];
-            if(sum==k) c++;
-            if(tmap.count(sum-k)>0) c+=tmap[sum-k];
-            if((tmap.count(sum)>0)) tmap[sum] = tmap[sum]+1;
-            else tmap[sum]=1;
+int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int sum=0,ans=0;
+        mp[sum] = 1;
+        for(auto it:nums){
+            sum += it;
+            int find = sum - k;
+            if(mp.find(find) != mp.end()){
+                ans += mp[find];
+            }
+            mp[sum]++;
         }
-        return c;
-        
+        return ans;
+
     }
 };
