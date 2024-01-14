2c16014840177cea0108fce4820a08d98ecee9cb
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-01-14 12:34:24 +0000

    Sync LeetCode submission - Rotate Array (cpp)

--- my-folder/problems/rotate_array/solution.cpp
+++ my-folder/problems/rotate_array/solution.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
-    void rotate(vector<int>& arr, int k) {
-        int n= arr.size();
-        vector<int> temp(n,0);
-        k=k%n;
-        for(int i=0;i<k;i++)
-        {
-            temp[i]=arr[n-k+i];
-        }
-        for(int i =0;i<n-k;i++)
-        {
-            temp[k+i]=arr[i];
-        }
-        arr=temp;
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
     }
 };
