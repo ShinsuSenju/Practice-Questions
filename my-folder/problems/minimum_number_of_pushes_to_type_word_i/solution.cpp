0c6cfa3e15c4919c8176911d766916b5ff314b5c
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-01-23 16:30:16 +0000

    Sync LeetCode submission - Minimum Number of Pushes to Type Word I (cpp)

--- my-folder/problems/minimum_number_of_pushes_to_type_word_i/solution.cpp
+++ my-folder/problems/minimum_number_of_pushes_to_type_word_i/solution.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        if (word.size() <= 7)
+        if (word.size() <= 8)
             return word.size();
-        else if (word.size() <= 15)
+        else if (word.size() <= 16)
             return (word.size() - 8) * 2 + 8;
-        else if(word.size()<=23)
+        else if(word.size()<=24)
             return 8 + 16 + (word.size() - 16) * 3;
         else
             return 8 + 16 + 24 + (word.size()-24)*4;
