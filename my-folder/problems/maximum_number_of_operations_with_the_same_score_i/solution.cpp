0dc21568b2b1b70f1c14fb2e502620d6922eccbb
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-02-17 14:46:53 +0000

    Sync LeetCode submission - Maximum Number of Operations With the Same Score I (cpp)

---
+++ my-folder/problems/maximum_number_of_operations_with_the_same_score_i/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums) {
+        int sum=nums[0]+nums[1];
+        int count=1;
+        
+        for(int i=2;i<nums.size();i+=2){
+            if(nums[i]+nums[i+1]==sum) count++;
+            else break;
+           
+            
+            
+        }
+        return count;
+        
+    }
+};
