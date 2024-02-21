ca49bc474c98f7cf9f9d39f3849c5003d239264e
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-02-21 18:10:13 +0000

    Sync LeetCode submission - Spiral Matrix (cpp)

--- my-folder/problems/spiral_matrix/solution.cpp
+++ my-folder/problems/spiral_matrix/solution.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int top=0,left=0;
+        int bottom=n-1,right=m-1;
         vector<int> ans;
-        if (matrix.empty() || matrix[0].empty()) {
-            return ans;
-        }
-
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int left = 0, right = n - 1;
-        int top = 0, bottom = m - 1;
-
-        while (left <= right && top <= bottom) {
- 
-            for (int i = left; i <= right; i++) {
-                ans.push_back(matrix[top][i]);
-            }
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++) ans.push_back(matrix[top][i]);
             top++;
-
-
-            for (int i = top; i <= bottom; i++) {
-                ans.push_back(matrix[i][right]);
-            }
+            for(int i=top;i<=bottom;i++) ans.push_back(matrix[i][right]);
             right--;
-
-    
-            if (top <= bottom) {
-  
-                for (int i = right; i >= left; i--) {
-                    ans.push_back(matrix[bottom][i]);
-                }
+            if(top<=bottom){
+                for(int i=right;i>=left;i--) ans.push_back(matrix[bottom][i]);
                 bottom--;
-            }
-
-  
-            if (left <= right) {
-
-                for (int i = bottom; i >= top; i--) {
-                    ans.push_back(matrix[i][left]);
-                }
+            }            
+            if(left<=right){
+                for(int i=bottom;i>=top;i--) ans.push_back(matrix[i][left]);
                 left++;
             }
         }
-
         return ans;
+        
     }
 };
