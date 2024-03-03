bcade3c369bd2af520d97e38280e34e6ffda8480
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-03-03 15:10:46 +0000

    Sync LeetCode submission - Move Zeroes (cpp)

--- my-folder/problems/move_zeroes/solution.cpp
+++ my-folder/problems/move_zeroes/solution.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int start = 0;
-        int mid =0;
-        int end = nums.size()-1;
-        while(mid<=end){
-            if(nums[mid]!=0){
-                swap(nums[mid],nums[start++]);
-            }
-            mid++;
+        int low =0;
+        for(int i = 0;i<nums.size();i++ ){
+            if(nums[i]!=0) swap(nums[i],nums[low++]);
         }
-        
-        
-        
+
     }
 };
