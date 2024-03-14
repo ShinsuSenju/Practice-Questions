75bed7742e2824b84ffa80ead7ae36a3831e29cf
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-03-14 01:47:28 +0000

    Sync LeetCode submission Runtime - 48 ms (25.74%), Memory - 40 MB (11.58%)

---
+++ my-folder/0966-binary-subarrays-with-sum/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int,int> map;
+        int sum = 0;
+        int count = 0;
+        map[sum]=1;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            count+=map[sum-goal];
+            map[sum]++;
+        }
+        return count;
+    }
+};
