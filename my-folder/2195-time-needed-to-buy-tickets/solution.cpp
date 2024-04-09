e998d34b0834cdb9b212cbfde3531bf19a3c44ed
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-04-09 17:18:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.3 MB (88.27%)

---
+++ my-folder/2195-time-needed-to-buy-tickets/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time = 0;
+        int i = 0;
+        while(tickets[k] > 0) {
+            if(tickets[i] > 0) {
+                tickets[i]--;
+                time++;
+            }
+            if(tickets[k] == 0) break;
+            i = (i + 1) % tickets.size(); 
+        }
+        return time;
+    }
+};
+
