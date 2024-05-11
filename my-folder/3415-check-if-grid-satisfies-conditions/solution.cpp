dde7a1187a9fc26e274db7f961b5d99863c9f2f7
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-05-11 18:08:06 +0000

    Sync LeetCode submission Runtime - 16 ms (50.00%), Memory - 28.7 MB (50.00%)

---
+++ my-folder/3415-check-if-grid-satisfies-conditions/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool satisfiesConditions(vector<vector<int>>& grid) {
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(i+1<grid.size()){
+                    if(grid[i][j]!=grid[i+1][j]) return false;
+                }
+                if(j+1<grid[0].size()){
+                    if(grid[i][j]==grid[i][j+1]) return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+};
