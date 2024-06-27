ae58a983399020e8b2c024e6fe116168b451d771
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-06-27 17:59:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (93.10%)

--- my-folder/0078-subsets/solution.cpp
+++ my-folder/0078-subsets/solution.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    void generate(vector<int>& nums,int start,vector<int> &set,vector<vector<int>>& ans){
-        ans.push_back(set);
-        for(int i=start;i<nums.size();i++){
-            set.push_back(nums[i]);
-            generate(nums,i+1,set,ans);
-            set.pop_back();
+    void recursion(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp, int n){
+        if(n == nums.size()){
+            ans.push_back(temp);
+            return;
         }
+        temp.push_back(nums[n]);
+        recursion(nums, ans, temp, n+1);
+        temp.pop_back();
+        recursion(nums, ans, temp, n+1);
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> set;
-        generate(nums,0,set,ans);
+        vector<int> temp;
+        recursion(nums, ans,temp, 0);
         return ans;
-        
     }
 };
+
