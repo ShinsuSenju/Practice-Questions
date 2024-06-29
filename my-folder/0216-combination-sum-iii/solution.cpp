08b9f6ede9a4f825c8a075ad8564652598f8c011
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-06-29 18:30:00 +0000

    Sync LeetCode submission Runtime - 2 ms (52.24%), Memory - 8.5 MB (31.00%)

--- my-folder/0216-combination-sum-iii/solution.cpp
+++ my-folder/0216-combination-sum-iii/solution.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-
-    void combi(vector<vector<int>>& ans, vector<int>& subset, int idx, int k, int n){
-        if(k == 0 && n == 0){
-            ans.push_back(subset);
+    void recursion(vector<vector<int>> &ans, vector<int> temp, int sum, int i, int k, int n){
+        if (sum > n || temp.size() > k) return;
+        if (sum == n && temp.size() == k) {
+            ans.push_back(temp);
+            return;
         }
-
-        for(int i = idx;i<10;i++){
-            if(i > idx && i == i-1) continue;
-            if(n == 0) break;
-            subset.push_back(i);
-            combi(ans, subset, i+1, k-1, n-i);
-            subset.pop_back(); 
+        for (int j = i; j <= 9; j++) {
+            temp.push_back(j);
+            recursion(ans, temp, sum + j, j + 1, k, n);
+            temp.pop_back(); 
         }
     }
+
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
-        vector<int> subset;
-
-        combi(ans, subset, 1, k, n);
+        recursion(ans, {}, 0, 1, k, n);
         return ans;
-        
     }
 };
