c7d5b8ae684b1ce602488699b2059a08cfb2b906
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-09-10 06:31:19 +0000

    Sync LeetCode submission Runtime - 57 ms (77.02%), Memory - 42.4 MB (88.59%)

---
+++ my-folder/1890-sum-of-beauty-of-all-substrings/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int beautySum(String s) {
+        int ans = 0;
+        for(int i=0;i<s.length();i++){
+            int[] freq = new int[26];
+            for(int j = i;j<s.length();j++){
+                freq[s.charAt(j)-'a']++;
+                int maxi = Integer.MIN_VALUE;
+                int mini = Integer.MAX_VALUE;
+                for(int k=0;k<26;k++){
+                    if(freq[k]>0){
+                        mini = Math.min(mini,freq[k]);
+                        maxi = Math.max(maxi,freq[k]);
+                    }
+                }
+                ans+=maxi-mini;
+            }
+        }
+        return ans;
+    }
+}
