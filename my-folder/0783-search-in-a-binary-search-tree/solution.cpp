2edc979773c44d2ce263db6e7e8d6eca73a01410
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-09-24 00:48:30 +0000

    Sync LeetCode submission Runtime - 33 ms (35.15%), Memory - 33.6 MB (59.05%)

---
+++ my-folder/0783-search-in-a-binary-search-tree/solution.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode*  cur = root;
+        while(cur!=nullptr){
+            if(cur->val==val) return cur;
+            else if(cur->val<val) cur = cur->right;
+            else cur = cur->left;
+        }
+        return nullptr;
+        
+    }
+};
