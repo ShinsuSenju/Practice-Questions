bc03d662d9dfe296a9d8095969eb2a23f2db5538
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-09-29 18:44:20 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 42.1 MB (91.46%)

--- my-folder/0008-string-to-integer-atoi/solution.java
+++ my-folder/0008-string-to-integer-atoi/solution.java
@@ -1,25 +1,26 @@
 class Solution {
+    long solve(String s, int i, long sum) {
+        if (i >= s.length() || !Character.isDigit(s.charAt(i))) return sum;
+        sum = sum * 10 + (s.charAt(i) - '0');
+        if (sum > Integer.MAX_VALUE) return sum;
+        return solve(s, i + 1, sum);
+    }
     public int myAtoi(String s) {
-        int sign = 1;
+        if (s == null || s.length() == 0) return 0;
         int i = 0;
-        int n = s.length();
-        if (n == 0) return 0;
-        while (i < n && s.charAt(i) == ' ') i++;
-        if (i == n) return 0;
+        while (i < s.length() && s.charAt(i) == ' ') i++;
+        if (i >= s.length()) return 0;
+        int sign = 1;
         if (s.charAt(i) == '-') {
             sign = -1;
             i++;
         } else if (s.charAt(i) == '+') {
             i++;
         }
-        long ans = 0;
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            ans = ans * 10 + (s.charAt(i) - '0');
-            if (sign == 1 && ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (sign == -1 && -ans < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            i++;
+        long ans = solve(s, i, 0);
+        if (ans > Integer.MAX_VALUE) {
+            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
         }
-        
         return (int) (ans * sign);
     }
 }
