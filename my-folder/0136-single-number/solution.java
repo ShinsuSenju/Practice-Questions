be8f9fa74929607ec2c8163296663b5a3fbf52b3
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-11-14 17:47:40 +0000

    Sync LeetCode submission Runtime - 1 ms (99.85%), Memory - 46.6 MB (6.54%)

--- my-folder/0136-single-number/solution.java
+++ my-folder/0136-single-number/solution.java
@@ -1,10 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor = 0;
-        for(int i=0;i<nums.length;i++){
-            xor^=nums[i];
+        int odd = 0;
+        for(int i:nums){
+            odd^=i;
         }
-        return xor;
+        return odd;
+
         
     }
 }
