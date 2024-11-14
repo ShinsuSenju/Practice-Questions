d13a54b886cf23a168002d2556d9ecd3bf9257b9
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2024-11-14 17:58:38 +0000

    Sync LeetCode submission Runtime - 3 ms (30.74%), Memory - 8.9 MB (42.40%)

--- my-folder/0078-subsets/solution.cpp
+++ my-folder/0078-subsets/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    void recursion(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp, int n){
-        if(n == nums.size()){
-            ans.push_back(temp);
-            return;
+    void findSubset(vector<int>& nums,vector<vector<int>>& ans,vector<int> temp, int ind){
+        ans.push_back(temp);
+        for(int i=ind;i<nums.size();i++){
+            temp.push_back(nums[i]);
+            findSubset(nums,ans,temp,i+1);
+            temp.pop_back();
         }
-        temp.push_back(nums[n]);
-        recursion(nums, ans, temp, n+1);
-        temp.pop_back();
-        recursion(nums, ans, temp, n+1);
-    }
 
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        recursion(nums, ans,temp, 0);
+        vector<vector<int>> ans ;
+        findSubset(nums,ans,{},0);
         return ans;
+
+        
     }
 };
-
