01e7c6ecaba1a8c653adea9e7d1ae5b375db492f
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-01-01 16:39:11 +0000

    Sync LeetCode submission Runtime - 3 ms (80.01%), Memory - 28.5 MB (42.57%)

--- my-folder/0503-next-greater-element-ii/solution.cpp
+++ my-folder/0503-next-greater-element-ii/solution.cpp
@@ -1,18 +1,17 @@
-class Solution
-{
-    public:
-        vector<int> nextGreaterElements(vector<int> &nums)
-        {
-            int n=nums.size();
-            vector<int> ans(n, -1);
-            stack<int> st;
-            for (int i = 2*n-1;i>=0;i--){
-                while (!st.empty() && st.top()<=nums[i % n]) st.pop();
-                if (i<n){
-                    if (!st.empty()) ans[i] = st.top();
-                }
-                st.push(nums[i%n]);
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        vector<int> ans(nums.size(),-1);
+        for(int i = 2*nums.size()-1;i>=0;i--){
+            while(!st.empty() && nums[i%nums.size()]>=st.top()) st.pop();
+            if(i<nums.size()){
+                if(!st.empty()) ans[i] = st.top();
+                
             }
-            return ans;
+            st.push(nums[i%nums.size()]);
+            
         }
+        return ans;
+    }
 };
