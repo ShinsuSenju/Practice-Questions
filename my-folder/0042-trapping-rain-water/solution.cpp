be2b9e6bb31e4d818cdfc27a391457ae28dc3967
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-01-02 09:58:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.4 MB (6.40%)

--- my-folder/0042-trapping-rain-water/solution.cpp
+++ my-folder/0042-trapping-rain-water/solution.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int total = 0, leftMax = 0, rightMax = 0;
-        int left = 0, right = height.size() - 1;
-        
-        while (left < right) {
-            if (height[left] <= height[right]) {
-                if (height[left] < leftMax) {
-                    total += leftMax - height[left];
-                } else {
-                    leftMax = height[left];
-                }
-                left++;
-            } else {
-                if (height[right] < rightMax) {
-                    total += rightMax - height[right];
-                } else {
-                    rightMax = height[right];
-                }
-                right--;
+        int n = height.size();
+        vector<int> pre(n),post(n);
+        for(int i = 0;i<n;i++){
+            if(i==0) pre[i]=height[i];
+            else{
+                if(height[i]>pre[i-1]) pre[i]=height[i];
+                else pre[i]=pre[i-1];
             }
         }
-        
+        for(int i = n-1;i>=0;i--){
+            if(i==n-1) post[i]=height[i];
+            else{
+                if(height[i]>post[i+1]) post[i]=height[i];
+                else post[i]=post[i+1];
+            }
+        }
+        int total = 0;
+        for(int i = 0 ;i<n;i++){
+           if(height[i]<pre[i] && height[i]<post[i]) total+=min(pre[i],post[i])-height[i];
+        }
         return total;
         
     }
