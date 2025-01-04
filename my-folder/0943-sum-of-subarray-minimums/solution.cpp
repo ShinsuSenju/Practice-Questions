9d2ff93054ac63e0598b3a04f9343b4c1c93423e
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-01-04 10:51:43 +0000

    Sync LeetCode submission Runtime - 14 ms (79.63%), Memory - 47 MB (63.45%)

--- my-folder/0943-sum-of-subarray-minimums/solution.cpp
+++ my-folder/0943-sum-of-subarray-minimums/solution.cpp
@@ -1,38 +1,31 @@
-class Solution {
-public:
-    int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> nextSmaller(n, n);
-        vector<int> prevSmaller(n, -1); 
-        stack<int> st;
-
-        for (int i = 0; i < arr.size(); i++) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
+class Solution
+{
+    public:
+        int sumSubarrayMins(vector<int> &arr)
+        {
+            int n = arr.size();
+            int mod = 1e9 + 7;
+            vector<int> preSmaller(n,-1);
+            vector<int> nextSmaller(n,n);
+            stack<int> st;
+            for(int i = 0;i<n;i++){
+                while(!st.empty() && arr[i]<arr[st.top()]) st.pop();
+                if(!st.empty()) preSmaller[i]=st.top();
+                st.push(i);
             }
-            if (!st.empty()) prevSmaller[i] = st.top();
-            st.push(i);
-        }
-
-        while (!st.empty()) st.pop();
-
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] > arr[i]) {
-                st.pop();
+            while(!st.empty()) st.pop();
+            for(int i = n-1;i>=0;i--){
+                while(!st.empty() && arr[i]<=arr[st.top()]) st.pop();
+                if(!st.empty()) nextSmaller[i]=st.top();
+                st.push(i);
             }
-            if (!st.empty()) nextSmaller[i] = st.top();
-            st.push(i);
+            long long total = 0;
+            for(int i = 0;i<n;i++){
+                long long left = i-preSmaller[i];
+                long long right = nextSmaller[i]-i;
+                total+=left*right*arr[i];
+                total%=mod;
+            }
+            return (int)total;
         }
-        long long total = 0;
-        int mod = 1e9 + 7;
-        for (int i = 0; i < arr.size(); i++) {
-            long long right = nextSmaller[i] - i;
-            long long left = i - prevSmaller[i];
-            total+=left*right*arr[i];
-            total%=mod;
-        }
-
-        return (int)total;
-    }
 };
-
