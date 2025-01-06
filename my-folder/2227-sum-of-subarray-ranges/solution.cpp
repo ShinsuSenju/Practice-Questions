18c038c3af489b38221bbe37fdbcf25411cb7c9f
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-01-06 07:08:24 +0000

    Sync LeetCode submission Runtime - 250 ms (5.82%), Memory - 16.5 MB (7.26%)

--- my-folder/2227-sum-of-subarray-ranges/solution.cpp
+++ my-folder/2227-sum-of-subarray-ranges/solution.cpp
@@ -1,19 +1,71 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long ans = 0;
-        for(int i = 0;i<nums.size();i++){
-            int mine = INT_MAX;
-            int maxe = INT_MIN;
-            for(int j = i;j<nums.size();j++){
-                mine = min(nums[j],mine);
-                maxe = max(nums[j],maxe); 
-                ans+=maxe - mine;
-            }
-            
+        int n = nums.size();
+        auto print = [](auto vec) {
+            for_each(vec.begin(), vec.end(), [](auto x) { cout << x << " "; });
+            cout << endl;
+        };
+        /*Sum of Subarrays Minimum */
+        vector<int> prevSmaller(n, -1), nextSmaller(n, n);
+        stack<int> temp;
+        for (int i = 0; i < n; i++) {
+            while (!temp.empty() && nums[temp.top()] > nums[i])
+                temp.pop();
+            if (!temp.empty())
+                prevSmaller[i] = temp.top();
+            temp.push(i);
         }
-        return ans;
+        while (!temp.empty())
+            temp.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!temp.empty() && nums[temp.top()] >= nums[i])
+                temp.pop();
+            if (!temp.empty())
+                nextSmaller[i] = temp.top();
+            temp.push(i);
+        }
+        print(prevSmaller);
+        print(nextSmaller);
+        while (!temp.empty())
+            temp.pop();
+        long long minTotal = 0;
+        for (int i = 0; i < n; i++) {
+            int left = i - prevSmaller[i];
+            int right = nextSmaller[i] - i;
+            minTotal += (long long)left * right * nums[i];
+        }
+        cout << minTotal << endl;
 
-        
+        /*Sum of Subarrays Maximum */
+        vector<int> prevGreater(n, -1), nextGreater(n, n);
+        for (int i = 0; i < n; i++) {
+            while (!temp.empty() && nums[temp.top()] < nums[i])
+                temp.pop();
+            if (!temp.empty())
+                prevGreater[i] = temp.top();
+            temp.push(i);
+        }
+        while (!temp.empty())
+            temp.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!temp.empty() && nums[temp.top()] <= nums[i])
+                temp.pop();
+            if (!temp.empty())
+                nextGreater[i] = temp.top();
+            temp.push(i);
+        }
+        while (!temp.empty())
+            temp.pop();
+        print(prevGreater);
+        print(nextGreater);
+        long long maxTotal=0;
+        for (int i = 0; i < n; i++) {
+            int left = i - prevGreater[i];
+            int right = nextGreater[i] - i;
+            maxTotal += (long long)left * right * nums[i];
+        }
+        cout << maxTotal << endl;
+        return maxTotal - minTotal;
     }
 };
