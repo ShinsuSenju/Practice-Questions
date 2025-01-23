7ca80dfc810a1dbfae51d15d88ecb15404b93dda
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-01-23 17:39:32 +0000

    Sync LeetCode submission Runtime - 2 ms (60.42%), Memory - 32.6 MB (80.59%)

--- my-folder/0966-binary-subarrays-with-sum/solution.cpp
+++ my-folder/0966-binary-subarrays-with-sum/solution.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int> map;
+    int helperFunc(vector<int>& nums,int goal){
+        if(goal<0) return 0;
+        int left = 0,right = 0;
         int sum = 0;
         int count = 0;
-        map[sum]=1;
-        for(int i=0;i<nums.size();i++){
-            sum += nums[i];
-            count+=map[sum-goal];
-            map[sum]++;
+        while(right<nums.size()){
+            sum+=nums[right++];
+            while(sum>goal) sum-=nums[left++];
+            count+= right-left;
         }
         return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        
+        return helperFunc(nums,goal)-helperFunc(nums,goal-1);
+        
     }
 };
