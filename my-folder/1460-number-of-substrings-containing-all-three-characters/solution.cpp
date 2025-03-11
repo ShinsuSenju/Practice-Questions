215443ecf6c6431bcb1ca9c4a2562da069315872
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-03-11 16:19:44 +0000

    Sync LeetCode submission Runtime - 4 ms (82.57%), Memory - 11.1 MB (35.02%)

--- my-folder/1460-number-of-substrings-containing-all-three-characters/solution.cpp
+++ my-folder/1460-number-of-substrings-containing-all-three-characters/solution.cpp
@@ -1,15 +1,19 @@
-class Solution
-{       
-    public:
-        int numberOfSubstrings(string s){
-            int lastSeen[3] = { -1,-1,-1 };
-            int i = 0;
-            int count = 0;
-            while (i < s.size()){
-                lastSeen[s[i] - 'a'] = i;
-                count+=1+min(lastSeen[0],min(lastSeen[1],lastSeen[2]));
-                i++;
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int left = 0;
+        vector<int> map(3,0);
+        int count = 0;
+        for(int right = 0;right<s.size();right++){
+            map[s[right]-'a']++;
+            while(map['a'-'a'] && map['b'-'a'] && map['c'-'a']){
+                count+=(s.size()-right);
+                map[s[left]-'a']--;
+                left++;
             }
-            return count;
+
         }
+        return count;
+        
+    }
 };
