c4438c31b22081fef3bf3adf4ae3266b0be1a711
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-06-23 12:44:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42 MB (67.91%)

--- my-folder/0075-sort-colors/solution.java
+++ my-folder/0075-sort-colors/solution.java
@@ -1,23 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0,mid = 0,high = nums.length-1;
-        while(mid<=high){
-            if(nums[mid]==0){
+        int left = 0, mid = 0, right = nums.length - 1;
+
+        while (mid <= right) {
+            if (nums[mid] == 0) {
                 int temp = nums[mid];
-                nums[mid] = nums[low];
-                nums[low] = temp;
-                low++;
+                nums[mid] = nums[left];
+                nums[left] = temp;
+                left++;
+                mid++;
+            } else if (nums[mid] == 2) {
+                int temp = nums[mid];
+                nums[mid] = nums[right];
+                nums[right] = temp;
+                right--;
+               
+            } else {
                 mid++;
             }
-            else if(nums[mid]==2){
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-                high--;
-            }
-            else mid++;
         }
-
-        
     }
 }
+
