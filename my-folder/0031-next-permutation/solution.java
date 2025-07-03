30105b3649ff278c7d67dcbf3e39812df5ab4ab7
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-07-03 18:51:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (88.02%)

--- my-folder/0031-next-permutation/solution.java
+++ my-folder/0031-next-permutation/solution.java
@@ -1,32 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
+        int idx1 = -1;
+        int idx2 = -1;
 
-        if (i >= 0) {
-            int j = nums.length - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
-                j--;
+        for(int i = nums.length - 1; i > 0; i--) {
+            if(nums[i] > nums[i - 1]) {
+                idx1 = i - 1;
+                break;
             }
-            swap(nums, i, j);
         }
-        reverse(nums, i + 1);
+
+        if(idx1 == -1) {
+            reverse(nums, 0);
+            return;
+        }
+
+        for(int i = nums.length - 1; i > idx1; i--) {
+            if(nums[i] > nums[idx1]) {
+                idx2 = i;
+                break;
+            }
+        }
+
+        swap(nums, idx1, idx2);
+        reverse(nums, idx1 + 1);
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 
-    private void reverse(int[] nums, int start) {
-        int end = nums.length - 1;
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
+    void reverse(int[] nums, int start) {
+        int i = start;
+        int j = nums.length - 1;
+        while(i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
         }
     }
 }
