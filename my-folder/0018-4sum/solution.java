262fcb72af406dd80c4bcaf14f29704d7ef3c8c8
Anurag Singh <63202411+ShinsuSenju@users.noreply.github.com>
2025-07-22 14:27:02 +0000

    Sync LeetCode submission Runtime - 13 ms (92.74%), Memory - 43.6 MB (98.49%)

--- my-folder/0018-4sum/solution.java
+++ my-folder/0018-4sum/solution.java
@@ -1,36 +1,31 @@
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        
-        for (int i = 0; i < nums.length - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; 
-
-            for (int j = i + 1; j < nums.length - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue; 
-                
-                int low = j + 1;
-                int high = nums.length - 1;
-
-                while (low < high) {
-                    long sum = (long)nums[i] + (long)nums[j] +(long)nums[low] + (long)nums[high];
-
-                    if (sum < target) {
-                        low++;
-                    } else if (sum > target) {
-                        high--;
-                    } else {
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[low], nums[high]));
-                        low++;
-                        high--;
-                        while (low < high && nums[low] == nums[low - 1]) low++;
-                        while (low < high && nums[high] == nums[high + 1]) high--;
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                long sum = nums[i]+nums[j];
+                int x = j+1;
+                int y = n-1;
+                while(x<y){
+                    long sum2 = nums[x]+nums[y];
+                    if(sum+sum2>target) y--;
+                    else if(sum+sum2<target) x++;
+                    else{
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[x],nums[y]));
+                        x++;
+                        y--;
+                        while(x<y && nums[x]==nums[x-1])x++;
+                        while(x<y && nums[y]==nums[y+1])y--;
                     }
                 }
+
             }
         }
         return ans;
+        
     }
 }
-
